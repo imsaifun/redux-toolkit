5328611be736beb8dd7b93f5afc0a4c76059d225
imsaifun <imsaifun@gmail.com>
2022-09-21 14:47:26 +0600

    successfully api connected with redux

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -2,24 +2,24 @@
 import Link from 'next/link';
 
 
-const VideoItem = ({ video = {} }) => {
-
-    const { id, title, thumbnail, } = video;
+const ProductItem = ({ product = {} }) => {
+console.log(product);
+    const { _id, companyTitle, thumbnail, } = product;
     return (
         <>
             <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
                 <div className="w-full flex flex-col">
                     <div className="relative">
-                        <Link href={`${id}`}>
+                        {/* <Link href={`${id}`}>
                             <a>
-                                <img src={thumbnail} className="w-full h-auto" alt="Some video title" />
+                                <img src={thumbnail} className="w-full h-auto" alt="Some product companyTitle" />
                             </a>
-                        </Link>
+                        </Link> */}
 
-                        <Link href={`${id}`}>
+                        <Link href={`${_id}`}>
                             <a>
                                 <p className="text-slate-900 text-sm font-semibold">
-                                    {title}
+                                    {companyTitle}
                                 </p>
                             </a>
                         </Link>
@@ -30,7 +30,7 @@ const VideoItem = ({ video = {} }) => {
     );
 };
 
-export default VideoItem;
+export default ProductItem;
 
 
 
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,37 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import VideoItem from "../components/VideoItem";
+import ProductItem from "../components/ProductItem";
 import { fetchProducts } from "../redux/products/products";
 
 function Home() {
 	const dispatch = useDispatch()
 
 	const { products, isLoading, isError, error } = useSelector(state => state.products)
-	console.log(products);
+	// console.log(products);
 	// const { tags, search } = useSelector((state) => state.filter);
 
 	useEffect(() => {
 		dispatch(fetchProducts())
 	}, [dispatch])
 
-	// let content;
-	// if (isLoading) content = "Loading"
-	// if (!isLoading && isError) content = <div className="col-span-12">{error}</div>
-	// if (!isLoading && !isError && videos?.length === 0) {
-	// 	content = <div className="col-span-12">No Videos Found</div>
-	// }
-	// if (!isLoading && !isError && videos?.length > 0) {
-	// 	content = videos.map((video, i) => (
-	// 		<VideoItem video={video} key={i} />
-	// 	))
-	// }
+	let content;
+	if (isLoading) content = "Loading"
+	if (!isLoading && isError) content = <div className="col-span-12">{error}</div>
+	if (!isLoading && !isError && products.data?.length === 0) {
+		content = <div className="col-span-12">No products Found</div>
+	}
+	if (!isLoading && !isError && products.data?.length > 0) {
+		content = products.data.map((product, i) => (
+			<ProductItem product={product} key={i} />
+		))
+	}
 
 	return (
 		<>
 
 				<section className="pt-12">
 					<div className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]">
-						{/* {content} */}
+						{content}
 					</div>
 				</section>
 		</>
