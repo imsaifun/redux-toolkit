325190ba0a29961288ed5d587949cb1f40e534dd
imsaifun <imsaifun@gmail.com>
2022-09-21 14:37:50 +0600

    working

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { server } from '../config'
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: `${server}/api`,
 });
 
 export default axiosInstance;
